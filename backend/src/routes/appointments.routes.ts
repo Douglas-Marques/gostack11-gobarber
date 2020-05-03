7d1c566fa8390fa2729c9c0d6090fff12388aaba
Douglas-Marques <douglasmarques09@gmail.com>
2020-05-02 22:21:30 -0300

    first commit

---
+++ backend/src/routes/appointments.routes.ts
@@ -0,0 +1,35 @@
+import { Router } from 'express';
+import { getCustomRepository } from 'typeorm';
+import { parseISO } from 'date-fns';
+
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import CreateAppointmentService from '../services/CreateAppointmentService';
+
+const appointmentsRouter = Router();
+
+appointmentsRouter.get('/', async (req, res) => {
+  const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+  const appointments = await appointmentsRepository.find();
+  return res.json(appointments);
+});
+
+appointmentsRouter.post('/', async (req, res) => {
+  try {
+    const { provider_id, date } = req.body;
+
+    const parsedDate = parseISO(date);
+
+    const createAppointmentService = new CreateAppointmentService();
+
+    const appointment = await createAppointmentService.execute({
+      date: parsedDate,
+      provider_id,
+    });
+
+    return res.json(appointment);
+  } catch (error) {
+    return res.status(400).json({ error: error.message });
+  }
+});
+
+export default appointmentsRouter;

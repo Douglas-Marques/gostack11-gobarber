ea9fb5783fa4c1f514508c29e725ca142a01da3b
Douglas-Marques <douglasmarques09@gmail.com>
2020-05-26 17:24:31 -0300

    Refactor: Modifying to use DDD architecture.

---
+++ backend/src/modules/users/infra/http/routes/users.routes.ts
@@ -0,0 +1,24 @@
+import { Router } from 'express';
+import multer from 'multer';
+import uploadConfig from '@config/upload';
+
+import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
+import UsersController from '@modules/users/infra/http/controllers/UsersController';
+import UsersAvatarController from '@modules/users/infra/http/controllers/UsersAvatarController';
+
+const usersRouter = Router();
+const usersController = new UsersController();
+const usersAvatarController = new UsersAvatarController();
+
+const upload = multer(uploadConfig);
+
+usersRouter.post('/', usersController.create);
+
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  usersAvatarController.update,
+);
+
+export default usersRouter;

f864b792867f13eb7363741db25a358d0fff8750
Douglas-Marques <douglasmarques09@gmail.com>
2020-09-10 23:34:30 -0300

    Feature: Adding S3 bucket

--- backend/src/config/upload.ts
+++ backend/src/config/upload.ts
@@ -1,19 +1,46 @@
 import path from 'path';
 import crypto from 'crypto';
-import multer from 'multer';
+import multer, { StorageEngine } from 'multer';
 
 const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp');
 
-export default {
-  tmpFolder: tmpFolder,
-  uploadFolder: path.resolve(tmpFolder, 'uploads'),
-  storage: multer.diskStorage({
-    destination: tmpFolder,
-    filename(req, file, cb) {
-      const fileHash = crypto.randomBytes(10).toString('HEX');
-      const filename = `${fileHash}-${file.originalname}`;
+interface IUploadConfig {
+  driver: 's3' | 'disk';
+  tmpFolder: string;
+  uploadFolder: string;
+  multer: {
+    storage: StorageEngine;
+  };
+  config: {
+    disk: {};
+    aws: {
+      bucket: string;
+    };
+  };
+}
 
-      return cb(null, filename);
+export default {
+  driver: process.env.STORAGE_DRIVER,
+
+  tmpFolder,
+  uploadFolder: path.resolve(tmpFolder, 'uploads'),
+
+  multer: {
+    storage: multer.diskStorage({
+      destination: tmpFolder,
+      filename(req, file, cb) {
+        const fileHash = crypto.randomBytes(10).toString('HEX');
+        const filename = `${fileHash}-${file.originalname}`;
+
+        return cb(null, filename);
+      },
+    }),
+  },
+
+  config: {
+    disk: {},
+    aws: {
+      bucket: 'app-gobarber.dg',
     },
-  }),
-};
+  },
+} as IUploadConfig;
